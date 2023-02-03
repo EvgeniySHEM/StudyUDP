f0cbdc79e2de1f1e4dc44686ebd86c390b9810b7
Evgeniy Sharychenkov <sanctio@yandex.ru>
2023-02-03 20:39:02 +0300

    Second commit

--- src/ru/sanctio/studyudp/UDPClient.java
+++ src/ru/sanctio/studyudp/UDPClient.java
@@ -1,9 +1,8 @@
 package ru.sanctio.studyudp;
 
-import java.io.DataInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
 
 public class UDPClient {
     private DatagramSocket socket;
@@ -12,63 +11,133 @@ public class UDPClient {
     private String fileName;
 
     public static void main(String[] args) {
-        try (DatagramSocket s = new DatagramSocket()){
+        System.out.println("Client started...");
+        try (DatagramSocket s = new DatagramSocket()) {
             UDPClient client = new UDPClient();
             client.socket = s;
-            String fileName = client.getFileName(); //запрашивается имя файла
-            client.sendFileName(fileName); //имя файла отправляется на сервер для проверки наличия
-            int size = client.getFileSize(); //сервер возвращает размер файла, если он есть
-            boolean b = client.getFile(size); //считываем и записываем файл отправленный сервером
-            if(b) {
-                System.out.println("File \"" + fileName + "\" is ready");
+            /**
+             * Запрос у пользователя имени файла для загрузки.
+             */
+            client.getFileName();
+            /**
+             * Запрос файла с сервера.
+             */
+            client.sendFileName();
+            /**
+             * Чтение ответа сервера, содержащего либо размер файла, либо код
+             * ERROR, который означает отсутствие запрошенного файла на сервере.
+             */
+            long size = client.getFileSize();
+            if (size > UDPServer.ERROR) {
+                try {
+                    /**
+                     * Загрузка файла с сервера.
+                     */
+                    client.getFile(size);
+                    System.out.println("File \"" + client.fileName
+                            + "\" is downloaded successfully.");
+                } catch (IOException e) {
+                    /**
+                     * Произошла ошибка чтения файла.
+                     */
+                    System.err.println("File \"" + client.fileName
+                            + "\" isn\'t downloaded.\n" + e.getMessage());
+                }
             } else {
-                System.out.println("File \"" + fileName + "\" not exists");
+                /**
+                 * Сервер вернул код ERROR, который означает, что на сервере нет
+                 * файла с запрошенным именем.
+                 */
+                System.out.println("File \"" + client.fileName + "\" not found");
             }
-        } catch (SocketException e) {
-            System.out.println("Error #1: " + e.getMessage());
+        } catch (SocketException ex) {
+            System.out.println("Error #1: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("Error #2: " + ex.getMessage());
+        }
+        System.out.println("Client stoped.");
+    }
+
+    /**
+     * Чтение имени запрашиваемого файла с консоли.
+     *
+     * @return строку с именем файла.
+     */
+    private void getFileName() {
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(System.in));) {
+            /**
+             * Цикл запроса имени файла должен выполняться до тех пор, пока
+             * пользователь не введёт непустое имя файла.
+             */
+            do {
+                System.out.println(">>> Input file name for download:");
+                fileName = in.readLine();
+            } while (fileName == null || fileName.trim().isEmpty());
         } catch (IOException e) {
-            System.out.println("Error #2: " + e.getMessage());
+            System.out.println("Errror: " + e.getMessage());
         }
+        System.out.println("Step 1");
     }
 
-    private boolean getFile(int size) throws IOException {
-        if(size != UDPServer.ERROR) {
-            buffer = new byte[size];
-            packet = new DatagramPacket(buffer, buffer.length);
-            socket.receive(packet);
-            try(FileOutputStream fos = new FileOutputStream(fileName)){
-                fos.write(buffer);
-            }
-            return true;
-        }
-        return false;
-    }
-
-    private int getFileSize() throws IOException {
-        buffer = new byte[4];
-        packet = new DatagramPacket(buffer, buffer.length);
-        socket.receive(packet);
-        DataInputStream dis = new DataInputStream(System.in);
-        int size = dis.read(buffer);
-        if(size != UDPServer.ERROR) {
-            buffer = UDPServer.OK.getBytes();
-            packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(UDPServer.UDP_SERVER_ADDRESS),
-                    UDPServer.UDP_SERVER_PORT);
-            socket.send(packet);
-            return size;
-        }
-        return size;
-    }
-    private void sendFileName(String fileName) throws IOException {
-        // ... проверки на валидность
+    /**
+     * Отправка запроса с именем файла.
+     *
+     * @param fileName строка с именем файла.
+     * @throws IOException в случае общей ошибки ввода/вывода.
+     */
+    private void sendFileName() throws IOException {
         buffer = fileName.getBytes();
-        packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(UDPServer.UDP_SERVER_ADDRESS),
+        /**
+         * В пакет заносится адрес сервера, его порт и сериализованное имя
+         * файла.
+         */
+        packet = new DatagramPacket(buffer, buffer.length,
+                InetAddress.getByName(UDPServer.UDP_SERVER_ADDRESS),
                 UDPServer.UDP_SERVER_PORT);
         socket.send(packet);
+        System.out.println("Step 2");
     }
 
-    private String getFileName() {
-        fileName = "test.txt";
-        return fileName;
+    /**
+     * Получение первого пакета от сервера с размером файла и отправка пакета с
+     * подтверждением о готовности принять файл.
+     *
+     * @return размер файла.
+     * @throws IOException в случае общей ошибки ввода/вывода.
+     */
+    private long getFileSize() throws IOException {
+        buffer = new byte[8];
+        packet = new DatagramPacket(buffer, buffer.length);
+        /**
+         * Получение пакета с размером файла.
+         */
+        socket.receive(packet);
+        long size = ByteBuffer.wrap(packet.getData()).getLong();
+        if (size > UDPServer.ERROR && size < Integer.MAX_VALUE) {
+            buffer = UDPServer.OK.getBytes();
+        } else {
+            buffer = UDPServer.NO.getBytes();
+        }
+        /**
+         * Подготовка и отправка пакета с подтверждением о готовности принять
+         * файл.
+         */
+        packet.setData(buffer);
+        packet.setLength(buffer.length);
+        socket.send(packet);
+        System.out.println("Step 3");
+        return size;
+    }
+
+    private void getFile(long size) throws IOException {
+        int s = size > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) size;
+        buffer = new byte[s];
+        packet.setData(buffer);
+        socket.receive(packet);
+        try (FileOutputStream fos = new FileOutputStream("Copy-" + fileName)) {
+            fos.write(buffer);
+        }
+        System.out.println("Step 4");
     }
 }
